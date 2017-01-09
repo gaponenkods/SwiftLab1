462ac278958c79000eeea4bfca7c57f1137de441
Konstantyn Byhkalo <Konstantyn@Konstantyns-MacBook-Pro.local>
2017-01-09 02:02:15 +0200

    SQLLite Models

--- Lab1/Models/SQLiteModels/ReadingModelSQL.swift
+++ Lab1/Models/SQLiteModels/ReadingModelSQL.swift
@@ -7,3 +7,35 @@
 //
 
 import Foundation
+import SQLite
+
+struct ReadingTable {
+    static let table = Table("readings")
+    
+    static let id = Expression<Int64>("reading_id")
+    static let timestamp = Expression<Int64>("timestamp")
+    static let value = Expression<Double>("name")
+    static let sensorId = Expression<Int64>("sensor_id")
+}
+
+class ReadingModelSQL {
+    
+    var readingId: Int64?
+    let timestamp: Int64
+    let value: Double
+    let sensorId: Int64
+    
+    init(reading: Row) {
+        readingId = reading[ReadingTable.id]
+        timestamp = reading[ReadingTable.timestamp]
+        value = reading[ReadingTable.value]
+        sensorId = reading[ReadingTable.sensorId]
+    }
+    
+    init(timestamp: Int64, value: Double, sensorId: Int64) {
+        self.timestamp = timestamp
+        self.value = value
+        self.sensorId = sensorId
+    }
+    
+}
--- Lab1/Models/SQLiteModels/SensorModelSQL.swift
+++ Lab1/Models/SQLiteModels/SensorModelSQL.swift
@@ -7,3 +7,26 @@
 //
 
 import Foundation
+import SQLite
+
+struct SensorTable {
+    static let table = Table("sensors")
+    
+    static let id = Expression<Int64>("sensor_id")
+    static let description = Expression<String>("sensorDescription")
+    static let name = Expression<String?>("name")
+}
+
+class SensorModelSQL {
+    
+    let sensorId: Int
+    let sensorDescription: String
+    let name: String
+    
+    init(sensor: Row) {
+        sensorId = Int(sensor[SensorTable.id])
+        sensorDescription = sensor[SensorTable.description]
+        name = sensor[SensorTable.name]!
+    }
+    
+}
